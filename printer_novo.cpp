f1f141c828f376fb093bbff4c1878cf2867c57ab
Victor <victorio.deg@gmail.com>
2024-11-26 23:45:11 -0300

    Ola

--- printer_novo.cpp
+++ printer_novo.cpp
@@ -4,7 +4,7 @@
 #include <unistd.h>
 #include <pthread.h>
 
-//semÃ¡foros
+//semáforos
 #include <semaphore.h>
 #include <fcntl.h>
 
@@ -15,42 +15,64 @@ void *imprime(void* args)
     int in = 0, out = 0;
 	while(1)
 	{
+		// Espera o semáforo ficar verde para continuar. Antes de entrar na 
+		// região crítica, coloca vermelho (0)
 		sem_wait(sem);
-		FILE* spool;
 
+		// ***Início da região crítica***, pois manipula arquivo compartilhado 
+		// com a printer
+
+		// Abre arquivo com informações do spooler, lendo variáveis in (índice 
+		// do arquivo a ser gravado) e out (índice do arquivo a ser impresso)
+		FILE* spool;
 		spool = fopen("spool.txt","r+");
 	    fscanf(spool, "%d %d\n", &in, &out);
 
+		// Incrementa out para ter índice do arquivo lido (o out sempre aponta 
+		// para o índice "anterior", de tal forma que, se ele é negativo, a 
+		// impressora sabe que já imprimiu tudo)
 		out++;
 		
+		// Se o out for válido, entra na rotina de impressão
 	    if(out < in && out >= 0)
 	    {
+			// Gera o nome do arquivo a imprimir
 			char filename[20];
-	
 			sprintf(filename, "%d.prt", out);
 	
+			// Abre arquivo a imprimir
             FILE *fout;
             fout = fopen(filename, "r");
 		
+			// Imprime arquivo
 			char content[100];
 			fscanf(fout, "%s", content);
 			printf("%s\n", content);
 
+			// Fecha arquivo e remove
 			fclose(fout);
 			remove(filename);
+
 		}else{
+
 			if(in == out)
 			    in = 0;
 			out = -1;
 			
 		}
 
+		// Atualiza spool com novos in e out (out incrementado para apontar 
+		// para o próximo arquivo a ser impresso)
 		rewind(spool);
     	fprintf(spool, "%d %d\n", in, out);		
-
 		fclose(spool);
+
+		// *** Fim da região crítica ***
+		// Passa semáforo para verde (1) para que outro processo que também
+		// utiliza esse sinal possa entrar na sua região crítica
 		sem_post(sem);
 		
+		// Se imprimiu tudo (out = -1), durma
 		//if(out == -1) raise(SIGSTOP);
 	}
 	return NULL;
@@ -69,12 +91,15 @@ int main()
 {
 
 	
+	// Pega número do processo e grava no arquivo para writer poder acordar o 
+	// printer
 	pid_t pid = getpid();
-	
 	FILE* spid = fopen("spid.txt", "w");
 	fprintf(spid, "%d", pid);
 	fclose(spid);
 	
+
+	// Cria semáforo, inicializando como verde (1)
 	sem = sem_open("/sem_print", O_CREAT | O_EXCL, 0644, 1);
 	if(sem == SEM_FAILED){
 	 	sem_unlink("/sem_print");
--- writer_novo.cpp
+++ writer_novo.cpp
@@ -3,7 +3,7 @@
 #include <signal.h>
 #include <unistd.h>
 
-//semÃ¡foros
+//semáforos
 #include <semaphore.h>
 #include <fcntl.h>
 
@@ -12,21 +12,35 @@ int main()
 
 	int in = 0, out = 0;
 
+    // Abre semáforo criado pelo printer
 	sem_t* sem = sem_open("/sem_print", O_RDWR);
 	if(sem == SEM_FAILED) exit(1);
 
 	while(1)
 	{
+		// Espera usuário digitar texto
 		char texto[100];
 		scanf("%s", texto);
 	
-
+		// Espera o semáforo ficar verde para continuar. Antes de entrar na 
+		// região crítica, coloca vermelho (0)
     	sem_wait(sem);
 	
+
     	FILE* spool;
+
+
+        // ***Início da região crítica***, pois manipula arquivo compartilhado 
+		// com a printer
+
+        // Abre arquivo com informações do spooler, lendo variáveis in (índice 
+		// do arquivo a ser gravado) e out (índice do arquivo a ser impresso)
 		spool = fopen("spool.txt","r+");		
 		fscanf(spool, "%d %d\n", &in, &out);
 		
+
+        // Cria arquivo de impressão com a string digitada pelo usuário.
+        // O nome do arquivo é igual ao índice de entrada do spooler
 		char filename[20];
 		sprintf(filename, "%d.prt", in);
 		
@@ -36,18 +50,22 @@ int main()
 		fprintf(fout, "%s", texto);
 		
 		fclose(fout);
-	
+
+		// Atualiza spooler com o índice da próxima posição livre
 		rewind(spool);
     	fprintf(spool, "%d %d\n", (in+1), out);
-		
 		fclose(spool);
 		sem_post(sem);
 	
+	    // Acorda processo printer lendo o seu número no arquivo
 		pid_t pid;
 		FILE* spid = fopen("spid.txt", "r");
 		fscanf(spid, "%d", &pid);
 		fclose(spid);
 
+        // *** Fim da região crítica ***
+        // passa semáforo para verde (1) para que outro processo que também 
+		// utiliza esse sinal possa entrar na sua região crítica
 		//kill(pid, SIGCONT);
 	}		
 
